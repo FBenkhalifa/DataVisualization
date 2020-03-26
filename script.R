668959845ab533a7b0c07c0de532959b61033ad7
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2020-03-26 15:33:31 +0100

    Add uni- and bivariate distributions

--- script.R
+++ script.R
@@ -35,18 +35,57 @@ mydata <- untable(df = mydata %>% select(-n),
 mydata %>% count(Class, Sex, Age, Survived, .drop = FALSE)
 
 # II Data exploration --------------------------------------------------------
-
-
 # Now distributions can easily be computed by choosing the variables of interest
+
+## A Univariate distributions
+
+# P(Sex)
+mydata %>% 
+  count(Sex, .drop = FALSE) %>% 
+  mutate(freq = n/sum(n))
+
+# P(Age)
+mydata %>% 
+  count(Age, .drop = FALSE) %>% 
+  mutate(freq = n/sum(n))
+
+# P(Class)
+mydata %>% 
+  count(Class, .drop = FALSE) %>% 
+  mutate(freq = n/sum(n))
+
+# P(Survived)
+mydata %>% 
+  count(Survived, .drop = FALSE) %>% 
+  mutate(freq = n/sum(n))
+
+
+## B Bivariate Distributions
+
+# P(Class, Sex)
+mydata %>% 
+  count(Class, Sex, .drop = FALSE) %>% 
+  mutate(freq= n/sum(n))
+
+# P(Class, Age)
+mydata %>% 
+  count(Class, Age, .drop = FALSE) %>% 
+  mutate(freq= n/sum(n))
+
+## C Conditinale Distributions
+
+# P(Sex|Class)
 mydata %>% 
   count(Class, Sex, .drop = FALSE) %>% 
   group_by(Class) %>% 
   mutate(freq_per_class = n/sum(n))
 
+# P(Class|Sex)
 mydata %>% 
   count(Class, Sex, .drop = FALSE) %>% 
   group_by(Sex) %>% 
   mutate(freq_per_Sex = n/sum(n))
+
 
 # III Data visualization ------------------------------------------------------
 
