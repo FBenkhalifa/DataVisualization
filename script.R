26739b45d818c7c99d0b8c2d53e2fb75ac8b8eac
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2020-03-26 13:37:27 +0100

    Set up repo

---
+++ script.R
@@ -0,0 +1,29 @@
+
+# Data wrangling
+library(tidyverse)
+library(lubridate)
+
+# Data viz
+library(plotly)
+library(ggthemes)
+library(cowplot)
+library(stargazer)
+library(skimr)
+
+# Data presentation
+library(knitr)
+
+# Data analysis
+library(tidymodels)
+library(broom)
+
+
+# I Data preparation -------------------------------------------------------------------------
+
+
+# II Data analysis --------------------------------------------------------
+
+
+# III Data visualization ------------------------------------------------------
+
+
