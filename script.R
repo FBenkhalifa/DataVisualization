63cf66137b8180ee2c7d2b044473e7ade9d17fcf
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2020-03-26 14:31:31 +0100

    Start with data analysis

--- script.R
+++ script.R
@@ -1,7 +1,8 @@
-
+library(datasets)
 # Data wrangling
 library(tidyverse)
 library(lubridate)
+library(reshape)
 
 # Data viz
 library(plotly)
@@ -20,9 +21,19 @@ library(broom)
 
 # I Data preparation -------------------------------------------------------------------------
 
+# Convert array to 2-dimensional format
+mydata <- Titanic %>% as_tibble()
 
-# II Data analysis --------------------------------------------------------
+# Convert all except n to factors which are easier to handle later on
+mydata <- mydata %>% mutate_at(vars(-n), as.factor) 
 
+# Untable the dbl which makes it easier to use with summarize functions
+mydata <- untable(df = mydata %>% select(-n), 
+                  num = mydata %>% pull(n))
+
+# II Data exploration --------------------------------------------------------
+mydata %>% group_by(Sex, Survived) %>% summarize(Cases = sum(n)) %>% group_by()
+mydata %>% group_by(Sex, Survived) %>% tally()
 
 # III Data visualization ------------------------------------------------------
 
