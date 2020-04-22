42a7b4f349dc9466e82ab0dfecc0c0ec4aa66f96
FBenkhalifa <florian.benkhalifa@student.unisg.ch>
2020-04-22 13:31:26 +0200

    Update plots

--- script.R
+++ script.R
@@ -6,6 +6,7 @@ library(reshape)
 
 # Data viz
 library(plotly)
+library(scales)
 library(ggthemes)
 library(cowplot)
 library(stargazer)
@@ -88,27 +89,68 @@ mydata %>%
 
 
 # III Data visualization ------------------------------------------------------
+who %>% pivot_longer(
+  cols = new_sp_m014:newrel_f65,
+  names_to = c("diagnosis", "gender", "age"),
+  names_pattern = "new_?(.*)_(.)(.*)",
+  values_to = "count"
+)
+## B Plot marginal distributions
+mydata_long <- mydata %>% pivot_longer(everything(), names_to = "variable", values_to = "attribute", names_repair = "universal")
+
+mydata_long %>% ggplot(aes(x = attribute, group = variable)) +
+  geom_bar(aes(y = (..count..)/sum(..count..)))+
+  scale_y_continuous(labels = percent) +
+  ylab("Survival Rate") +
+  facet_wrap(~variable, scales = "free_x") +
+  theme_hc()
+
+
+## A Plot data with target variable
+survival_rate <- mydata %>% 
+  count(Survived, .drop = FALSE) %>% 
+  mutate(freq_per_class = n/sum(n))
+
+# Convert data to long format for facet plotting
+mydata_semi_long <- mydata %>% pivot_longer(-Survived, names_to = "variable", values_to = "attribute", names_repair = "universal")
+
+mydata_semi_long %>% ggplot(aes(x = attribute, fill = Survived)) +
+  geom_bar(position = "fill") +
+  scale_y_continuous(labels = percent) +
+  ylab("Survival Rate") +
+  scale_fill_brewer(palette = "Set1") +
+  geom_hline(yintercept = survival_rate$freq_per_class[2], col = "white", lty = 2, size = 1.5) +
+  facet_wrap(~variable, scales = "free_x")
+
+
+
+# IV Model Fitting and evaluation ------------------------------------------------------
 
 # A Fit linear regression
-linear_reg <- 
-  mydata %>% 
-  # Convert target variable to numeric for linear regression
-  mutate_at("Survived", ~case_when(Survived == "No" ~ 0, 
-                                   Survived == "Yes" ~ 1)) %>%
-  # Run regression
-  lm(Survived ~ ., .)
+linear_reg <- mydata %>% 
+  mutate_at("Survived", ~case_when(Survived == "No" ~ 0,
+                                   Survived == "Yes" ~ 1)) %>%  # Convert target variable to numeric for linear regression
+  lm(Survived ~ ., .) # Run regression
 
-# Display coefficients and informations
+# Display coefficients and informations and summary statistics
 tidy(linear_reg)
-
-# Display summary statistics
 glance(linear_reg)
 
 # B Fit log regression
 log_reg <- glm(Survived ~ ., mydata, family = binomial(link = "logit"))
 
-# Display coefficients and informations
-tidy(linear_reg)
+# Display coefficients and informations and summary statistics 
+tidy(log_reg)
+glance(log_reg)
 
-# Display summary statistics
-glance(linear_reg)
+
+
+# B Compute anova
+
+# Produce linear anova
+linear_anova <- anova(linear_reg)
+linear_anova %>% tidy
+
+# Produce log anova
+log_anova <- anova(log_reg)
+log_anova %>% tidy
